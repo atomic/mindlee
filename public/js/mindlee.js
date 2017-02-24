6040593a002483ea3ba4b0583f6d77668abbf9d3
Tony Lim <atomictheorist@gmail.com>
2017-02-23 21:38:25 -0800

    add snackbar and background image opacity

--- public/js/mindlee.js
+++ public/js/mindlee.js
@@ -50,3 +50,14 @@ function deleteActivity(e) {
     });
     console.log('User clicked delete activity');
 }
+
+function showReminder() {
+    // Get the snackbar DIV
+    var x = document.getElementById("snackbar");
+
+    // Add the "show" class to DIV
+    x.className = "show";
+
+    // After 3 seconds, remove the show class from DIV
+    setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+}
