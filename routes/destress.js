f9b41251a1feed23bb23f2b4d8ed492dcd0d52ee
Tony Lim <atomictheorist@gmail.com>
2017-03-01 19:49:20 -0800

    De-stress screen layout working and Fix #4 using different hover effect

--- routes/destress.js
+++ routes/destress.js
@@ -1,7 +1,8 @@
 /**
  * Created by atomic on 2/9/17.
  */
+var destress = require('../destress.json');
 
 exports.view = function(req, res){
-    res.render('destress');
+    res.render('destress', { link:'/destress', title: 'Destress', 'destress' : destress});
 };
